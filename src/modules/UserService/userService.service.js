bd54ba8911da84c5f4a52d7b172e05a49ceac356
MoshPe <fghghf98@gmail.com>
2023-01-15 00:28:25 +0200

    add vercel client

--- src/modules/UserService/userService.service.js
+++ src/modules/UserService/userService.service.js
@@ -60,6 +60,7 @@ const login = async (req, res) => {
     res.status(200).json({
       success: true,
       message: 'Successfully authenticated',
+      foundUser,
       accessToken,
     });
   } else
