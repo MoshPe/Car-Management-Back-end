7f65f8f70dcc358727b7fd9bd0a598fcbbe74370
MoshPe <fghghf98@gmail.com>
2023-01-15 00:27:47 +0200

    add vercel client

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://car-managment-tm.vercel.app/',
+];
 
 module.exports = allowedOrigins;
